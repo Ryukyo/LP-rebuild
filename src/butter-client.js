7035f38ec85500381da75a85a817aa66f16416bf
Ryukyo <florian.fejer@hotmail.de>
2021-08-09 20:45:48 +0900

    Butter client and custom hook

---
+++ src/butter-client.js
@@ -0,0 +1,7 @@
+import Butter from 'buttercms'
+// require('dotenv').config()
+
+// loading a variable from .env file doesn't work in the current setup
+const butter = Butter("d2154a99d6ce17bb7f548715ffd8eb20a02fec87")
+
+export default butter
